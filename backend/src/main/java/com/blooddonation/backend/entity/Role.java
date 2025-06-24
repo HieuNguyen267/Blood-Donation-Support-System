faceab88cd4337a42643da8d958ad3bd97bac359
trieu-teach <trieungse182933@fpt.edu.vn>
2025-06-25 02:28:49 +0700

    Delete backend directory

--- backend/src/main/java/com/blooddonation/backend/entity/Role.java
+++
@@ -1,9 +0,0 @@
-package com.blooddonation.backend.entity;
-
-public enum Role {
-<<<<<<< HEAD
-    USER, ADMIN, STAFF, MEDICAL_FACILITY
-=======
-    USER, ADMIN
->>>>>>> 073bd8776b9e662f3d0df0a5cb949ba04fad2983
-} 
